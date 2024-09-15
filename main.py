5aa143ca07afab65e0e0d5d8a1a1c245278c2305
JoLiKs <vonkad002@gmail.com>
2024-09-16 00:17:22 +0300

    CRUD, routes, fixes

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import uvicorn
 from fastapi import FastAPI
+
+import routes
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(routes.router)
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
+
+
+
+
